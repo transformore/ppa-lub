d5e6ff84d70c121b8cc477d9ce1777d2ffed6b46
Mochammad Suwito <ss6transform@gmail.com>
2021-07-28 15:01:14 +0700

    Bounce animation on login screen

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -12,6 +12,7 @@ import {
   StyleSheet,
   TextInput,
   Dimensions,
+  Animated,
 } from 'react-native';
 import {
   // TextInput,
@@ -43,9 +44,18 @@ class LoginPage extends Component {
       loginMessageVisible: false,
       loginMessage: '',
       siteDialogVisible: false,
+      animation: new Animated.Value(-250),
     };
   }
-
+  componentDidMount = () => {
+    Animated.spring(this.state.animation, {
+      toValue: 0,
+      duration: 1500,
+      friction: 0.8,
+      tension: 7,
+      useNativeDriver: true,
+    }).start();
+  };
   login = () => {
     this.setState({loading: true});
     // this.context.setApprovalUpdate(true);
@@ -139,6 +149,9 @@ class LoginPage extends Component {
   hideSiteDialog = () => this.setState({siteDialogVisible: false});
 
   render() {
+    const trans = {
+      transform: [{translateY: this.state.animation}],
+    };
     return (
       <Fragment>
         <StatusBar
@@ -153,181 +166,183 @@ class LoginPage extends Component {
             justifyContent: 'center',
           }}>
           <ScrollView style={{backgroundColor: 'transparent'}}>
-            <View
-              style={{
-                margin: 10,
-                backgroundColor: Colors.clear,
-              }}>
+            <Animated.View style={[styles.ball, trans]}>
               <View
                 style={{
-                  marginTop: 0,
-                  flexDirection: 'row',
-                  justifyContent: 'center',
-                  alignItems: 'center',
+                  margin: 10,
+                  backgroundColor: Colors.clear,
                 }}>
-                <Image
-                  style={{
-                    height: 50,
-                    width: 50,
-                    resizeMode: 'contain',
-                  }}
-                  source={require('../assets/image/logoppa.png')}
-                />
                 <View
                   style={{
-                    marginLeft: 10,
-                    marginBottom: 5,
-                  }}
-                />
-              </View>
-              <View
-                style={{
-                  alignItems: 'center',
-                  marginTop: 15,
-                  marginBottom: 20,
-                }}>
-                <Text style={{fontSize: 35, fontWeight: 'bold'}}>PPA</Text>
-                <Text style={styles.logo}>Oil&Grease</Text>
-              </View>
-              <View style={{zIndex: 2}}>
-                <TouchableNativeFeedback
-                  delayPressIn={0}
-                  background={TouchableNativeFeedback.Ripple(Colors.green200)}
-                  onPress={() => {
-                    this.showSiteDialog();
-                    Keyboard.dismiss();
+                    marginTop: 0,
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                   }}>
+                  <Image
+                    style={{
+                      height: 50,
+                      width: 50,
+                      resizeMode: 'contain',
+                    }}
+                    source={require('../assets/image/logoppa.png')}
+                  />
                   <View
                     style={{
-                      alignItems: 'center',
-                      borderRadius: 25,
-                      paddingLeft: 30,
+                      marginLeft: 10,
+                      marginBottom: 5,
+                    }}
+                  />
+                </View>
+                <View
+                  style={{
+                    alignItems: 'center',
+                    marginTop: 15,
+                    marginBottom: 20,
+                  }}>
+                  <Text style={{fontSize: 35, fontWeight: 'bold'}}>PPA</Text>
+                  <Text style={styles.logo}>Oil&Grease</Text>
+                </View>
+                <View style={{zIndex: 2}}>
+                  <TouchableNativeFeedback
+                    delayPressIn={0}
+                    background={TouchableNativeFeedback.Ripple(Colors.green200)}
+                    onPress={() => {
+                      this.showSiteDialog();
+                      Keyboard.dismiss();
+                    }}>
+                    <View
+                      style={{
+                        alignItems: 'center',
+                        borderRadius: 25,
+                        paddingLeft: 30,
+                        marginHorizontal: 50,
+                        marginTop: 20,
+                        marginBottom: 10,
+                        height: 50,
+                        flexDirection: 'row',
+                        // backgroundColor: '#F4F4F4',
+                        backgroundColor: 'white',
+                      }}>
+                      <Text
+                        style={{
+                          fontSize: 18,
+                          textAlign: 'center',
+                          fontFamily: 'fontisto',
+                          color: 'grey',
+                        }}>
+                        {this.context.siteId != null ? 'SITE  ' : 'SITE  '}
+                      </Text>
+                      <Text style={{fontSize: 18, color: 'grey'}}>
+                        {this.context.siteId != null
+                          ? site[this.context.siteId].name
+                          : ''}
+                      </Text>
+                    </View>
+                  </TouchableNativeFeedback>
+
+                  <TextInput
+                    style={{
                       marginHorizontal: 50,
-                      marginTop: 20,
                       marginBottom: 10,
                       height: 50,
-                      flexDirection: 'row',
-                      // backgroundColor: '#F4F4F4',
-                      backgroundColor: 'white',
+                      backgroundColor: Colors.white,
+                      borderRadius: 25,
+                      fontSize: 18,
+                      paddingLeft: 30,
+                    }}
+                    mode="flat"
+                    label="NRP"
+                    value={this.state.nrp}
+                    onChangeText={(text) => this.setState({nrp: text})}
+                    keyboardType={'numeric'}
+                    placeholder={'NRP'}
+                  />
+                  <TextInput
+                    secureTextEntry
+                    style={{
+                      marginHorizontal: 50,
+                      marginBottom: 10,
+                      height: 50,
+                      backgroundColor: Colors.white,
+                      borderRadius: 25,
+                      paddingLeft: 30,
+                    }}
+                    mode="flat"
+                    label="PASSWORD"
+                    value={this.state.password}
+                    onChangeText={(text) => this.setState({password: text})}
+                    // keyboardType={"numeric"}
+                    keyboardType={'default'}
+                    placeholder={'PASSWORD'}
+                  />
+
+                  <List.Item
+                    title="Network Status"
+                    description={`Current network: ${
+                      this.context.isPublicNetwork ? 'Public' : 'Local'
+                    }`}
+                    left={(props) => (
+                      <List.Icon
+                        {...props}
+                        style={{marginLeft: 0}}
+                        icon={'wifi'}
+                      />
+                    )}
+                    right={() => (
+                      <Switch
+                        value={this.context.isPublicNetwork}
+                        onValueChange={() =>
+                          this.context.setNetwork(!this.context.isPublicNetwork)
+                        }
+                      />
+                    )}
+                    style={{
+                      // margin: 0,
+                      padding: 0,
+                      marginHorizontal: 40,
+                      marginTop: 10,
+                    }}
+                  />
+
+                  <Button
+                    loading={this.state.loading}
+                    dark
+                    // color={colors.primary}
+                    mode="contained"
+                    onPress={this.login}
+                    style={{
+                      width: 260,
+                      marginHorizontal: 50,
+                      marginTop: 20,
+                      paddingVertical: 8,
+                      fontSize: 25,
+                      borderRadius: 28,
+                      backgroundColor: '#F79F1F',
+                      alignSelf: 'center',
                     }}>
-                    <Text
-                      style={{
-                        fontSize: 18,
-                        textAlign: 'center',
-                        fontFamily: 'fontisto',
-                        color: 'grey',
-                      }}>
-                      {this.context.siteId != null ? 'SITE  ' : 'SITE  '}
-                    </Text>
-                    <Text style={{fontSize: 18, color: 'grey'}}>
-                      {this.context.siteId != null
-                        ? site[this.context.siteId].name
-                        : ''}
-                    </Text>
-                  </View>
-                </TouchableNativeFeedback>
-
-                <TextInput
+                    Login
+                  </Button>
+                </View>
+                <View
                   style={{
-                    marginHorizontal: 50,
-                    marginBottom: 10,
-                    height: 50,
-                    backgroundColor: Colors.white,
-                    borderRadius: 25,
-                    fontSize: 18,
-                    paddingLeft: 30,
-                  }}
-                  mode="flat"
-                  label="NRP"
-                  value={this.state.nrp}
-                  onChangeText={(text) => this.setState({nrp: text})}
-                  keyboardType={'numeric'}
-                  placeholder={'NRP'}
-                />
-                <TextInput
-                  secureTextEntry
-                  style={{
-                    marginHorizontal: 50,
-                    marginBottom: 10,
-                    height: 50,
-                    backgroundColor: Colors.white,
-                    borderRadius: 25,
-                    paddingLeft: 30,
-                  }}
-                  mode="flat"
-                  label="PASSWORD"
-                  value={this.state.password}
-                  onChangeText={(text) => this.setState({password: text})}
-                  // keyboardType={"numeric"}
-                  keyboardType={'default'}
-                  placeholder={'PASSWORD'}
-                />
-
-                <List.Item
-                  title="Network Status"
-                  description={`Current network: ${
-                    this.context.isPublicNetwork ? 'Public' : 'Local'
-                  }`}
-                  left={(props) => (
-                    <List.Icon
-                      {...props}
-                      style={{marginLeft: 0}}
-                      icon={'wifi'}
-                    />
-                  )}
-                  right={() => (
-                    <Switch
-                      value={this.context.isPublicNetwork}
-                      onValueChange={() =>
-                        this.context.setNetwork(!this.context.isPublicNetwork)
-                      }
-                    />
-                  )}
-                  style={{
-                    // margin: 0,
-                    padding: 0,
-                    marginHorizontal: 40,
-                    marginTop: 10,
-                  }}
-                />
-
-                <Button
-                  loading={this.state.loading}
-                  dark
-                  // color={colors.primary}
-                  mode="contained"
-                  onPress={this.login}
-                  style={{
-                    width: 260,
-                    marginHorizontal: 50,
-                    marginTop: 20,
-                    paddingVertical: 8,
-                    fontSize: 25,
-                    borderRadius: 28,
-                    backgroundColor: '#F79F1F',
-                    alignSelf: 'center',
+                    width: width - 20,
+                    height: 70,
+                    alignItems: 'center',
+                    justifyContent: 'flex-end',
                   }}>
-                  Login
-                </Button>
+                  <Text
+                    style={{
+                      color: Colors.grey500,
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                    }}>
+                    Ver {this.context.app_version}
+                  </Text>
+                </View>
               </View>
-              <View
-                style={{
-                  width: width - 20,
-                  height: 70,
-                  alignItems: 'center',
-                  justifyContent: 'flex-end',
-                }}>
-                <Text
-                  style={{
-                    color: Colors.grey500,
-                    fontSize: 14,
-                    fontWeight: 'bold',
-                  }}>
-                  Ver {this.context.app_version}
-                </Text>
-              </View>
-            </View>
-            <View style={{height: 500, backgroundColor: 'grey300'}} />
+              <View style={{height: 500, backgroundColor: 'grey300'}} />
+            </Animated.View>
           </ScrollView>
           <Portal>
             <Dialog
